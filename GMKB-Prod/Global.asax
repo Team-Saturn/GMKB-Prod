ca5e987917d27254bcb458429449333853cd9f41
am934743 <am934743@gmail.com>
2015-03-30 21:36:32 -0500

    Init Commit
    Init Commit

---
+++ GMKB-Prod/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GMKB_Prod.MvcApplication" Language="C#" %>
---
+++ GMKB-Prod/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GMKB_Prod.Startup))]
+namespace GMKB_Prod
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
